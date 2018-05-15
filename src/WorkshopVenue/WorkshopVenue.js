7504a47a22b8c60a66a35d57321c9145e296bd22
amyllz <xiaoxiaozhezi@163.com>
2018-05-15 17:51:50 +0800

    aa

--- src/WorkshopVenue/WorkshopVenue.js
+++ src/WorkshopVenue/WorkshopVenue.js
@@ -18,8 +18,8 @@ export default class WorkshopVenue extends Component {
           <div className='content'>
             <h2 className='title'>Workshop Venue</h2>
             <p>The workshop will be held at Hangzhou Jinxi Villa, a hotel surrounded by West Lake.</p>
-            <img src={require("./location.jpg")} style={{ width: "500px" }} alt="hangzhou" />
-            <img src={require("./location2.jpg")} style={{ width: "500px", marginTop: '14px' }} alt="hangzhou" />
+            <p><img src={require("./location.jpg")} style={{ width: "500px" }} alt="hangzhou" /></p>
+            <p><img src={require("./location2.jpg")} style={{ width: "500px", marginTop: '14px' }} alt="hangzhou" /></p>
             <p style={{ marginTop: '14px' }}>Workshop banquet will be held in the Shan Wai Shan Restaurant, located in Hangzhou Botanical Garden.</p>
             <img src={require("./rest.jpg")} style={{ width: "500px" }} alt="hangzhou" />
           </div>
